e1a492bb4d0aa69371367e315eead804d6485abe
rehitem <hubg2469>
2023-11-28 12:35:17 +0330

    fuvk you

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("hellobithc");
-Console.WriteLine( "gozo");
+Console.WriteLine( "gozo");er
